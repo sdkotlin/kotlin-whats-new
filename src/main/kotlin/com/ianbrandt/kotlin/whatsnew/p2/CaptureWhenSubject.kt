b47e6f83d965490ab144375864432980b19a873f
Ian Brandt <ian@ianbrandt.com>
2018-11-05 20:24:59 -0800

    Added when subject example.

---
+++ src/main/kotlin/com/ianbrandt/kotlin/whatsnew/p2/CaptureWhenSubject.kt
@@ -0,0 +1,21 @@
+package com.ianbrandt.kotlin.whatsnew.p2
+
+
+fun main() {
+
+	// Before...
+//	val theAnswer = 6
+//	when (theAnswer) {
+//		...
+//	}
+//	println(theAnswer)
+
+	// When subjects can now be declared in the expression itself:
+	when (val theAnswer = 6) {
+		42 -> println("Wait, what was the question?")
+		else -> println("$theAnswer?! 🤦‍")
+	}
+
+	// They are then scoped to it, so this does not compile...
+//	 println(theAnswer)
+}
